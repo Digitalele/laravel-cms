955dceba4278bf33ec2ef9057626d8c3bf05ab36
lele <lele@crackberry.local>
2017-11-30 19:12:20 +0100

    add 404 page error laravel is amazing

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-center">404 Not Found</h2>
+            </div>
+        </div>
+    </div>
+
+@endsection
